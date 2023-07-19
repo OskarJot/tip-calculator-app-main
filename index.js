3136d33e48c7d9eaf5b21b4ed830a1df69d997df
OskarJot <oskar.jarnot@gmail.com>
2023-07-19 22:57:18 +0200

    func get value from bill input

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log('Hello');
+
+const billValue = document.querySelector('.billValue');
+
+function getValueBill() {
+	console.log(billValue.value);
+}
+
+billValue.addEventListener('keyup', getValueBill);
