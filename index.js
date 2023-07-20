988c9bf5032ef8670eb750915caa4a0b945c66a5
OskarJot <oskar.jarnot@gmail.com>
2023-07-20 22:24:31 +0200

    get tip value  from button to log.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 console.log('Hello');
 
 const billValue = document.querySelector('.billValue');
+const buttonTip = document.querySelectorAll('.button-tip');
+
+buttonTip.forEach((button) => {
+	button.addEventListener('click', function () {
+		console.log(button.textContent);
+	});
+});
 
 function getValueBill() {
 	console.log(billValue.value);
