81fc985b7d48e94e23d7e8879af6f5a86274fa8a
OskarJot <oskar.jarnot@gmail.com>
2023-07-20 22:33:55 +0200

    get value number of people

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ console.log('Hello');
 
 const billValue = document.querySelector('.billValue');
 const buttonTip = document.querySelectorAll('.button-tip');
+const numPeople = document.querySelector('.peopleValue');
 
 buttonTip.forEach((button) => {
 	button.addEventListener('click', function () {
@@ -13,4 +14,9 @@ function getValueBill() {
 	console.log(billValue.value);
 }
 
+function getValuePeople() {
+	console.log(numPeople.value);
+}
+
 billValue.addEventListener('keyup', getValueBill);
+numPeople.addEventListener('keyup', getValuePeople);
